697a17ddab21dee2329639ff36fa70c6829b5735
Murad <murad-arslangiraev-1994@mail.ru>
2020-06-12 18:50:18 +0300

    Сделал верстку и разбил на компоненты

---
+++ src/components/PostForm.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+export default class PostForm extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            title: ''
+        }
+    }
+
+    submitHandler = event => {
+        event.preventDefault();
+        const {title} = this.state
+        
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+
+        console.log(newPost)
+        this.setState({title: ''})
+    }
+
+    changeInputHandler = event => {
+        event.persist()
+        this.setState(prev => ({...prev, ...{
+            [event.target.name]: event.target.value
+        }}))
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submitHandler}>
+                <div className="form-group">
+                    <label htmlFor="title">Заголовок поста</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="title"
+                        value={this.state.title}
+                        name="title"
+                        onChange={this.changeInputHandler}
+                    />
+                </div>
+                <button className="btn btn-success" type='submit'>Создать</button>
+            </form>
+        )
+    }
+}
---
+++ src/components/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Post from './Post'
+
+export default ({posts}) => {
+    if(!posts.length) {
+        return <p className="text-center">Постов нет</p>
+    }
+    return posts.map(post => <Post post={post} key={post}/>)
+}
