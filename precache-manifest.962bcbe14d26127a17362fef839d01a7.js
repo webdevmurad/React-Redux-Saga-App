4c79e762750ae847013f9ba4dbfc75ddf54cd159
Murad <murad-arslangiraev-1994@mail.ru>
2020-11-04 16:04:37 +0300

    Updates

---
+++ precache-manifest.962bcbe14d26127a17362fef839d01a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a4c8f9a430e9df575547273417e0ba",
+    "url": "/React-Redux-Saga-App/index.html"
+  },
+  {
+    "revision": "c374ee4b2f4abbec4457",
+    "url": "/React-Redux-Saga-App/static/js/2.d28ad5ec.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Redux-Saga-App/static/js/2.d28ad5ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dee7df0fa66a4a1080",
+    "url": "/React-Redux-Saga-App/static/js/main.2bf67923.chunk.js"
+  },
+  {
+    "revision": "b3c202ea711a285adf54",
+    "url": "/React-Redux-Saga-App/static/js/runtime-main.f9dc05e9.js"
+  }
+]);
